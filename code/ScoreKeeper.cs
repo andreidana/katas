cf4713ed792ffee46cf1828102ad3b542fb85743
Andrei Dana <andrei.dana87@gmail.com>
2021-01-07 23:49:46 +0200

    finish score keeper

--- code/ScoreKeeper.cs
+++ code/ScoreKeeper.cs
@@ -14,13 +14,57 @@ void scoreTeamB3()
 String getScore()
 Rules
 The returned String always has seven characters. An example would be 000:000*/
+using System;
+
 namespace katas
 {
     public class ScoreKeeper
     {
+        private int teamAScore;
+        private int teamBScore;
+
+        public ScoreKeeper()
+        {
+            teamAScore = 0;
+            teamBScore = 0;
+        }
+
         public string GetScore()
         {
-            return "000:000";
+            var aTeamScoreString = teamAScore.ToString().PadLeft(3, '0');
+            var bTeamScoreString = teamBScore.ToString().PadLeft(3, '0');
+            return $"{aTeamScoreString}:{bTeamScoreString}";
+        }
+
+
+        public void scoreTeamA1()
+        {
+            teamAScore += 1;
+        }
+
+        public void scoreTeamA2()
+        {
+            teamAScore += 2;
+        }
+
+        public void scoreTeamA3()
+        {
+            teamAScore += 3;
+        }
+
+        public void scoreTeamB1()
+        {
+            teamBScore += 1;
+        }
+
+        public void scoreTeamB2()
+        {
+            teamBScore += 2;
+        }
+
+        public void scoreTeamB3()
+        {
+            teamBScore += 3;
         }
     }
 }
--- tests/ScoreKepperTests.cs
+++ tests/ScoreKepperTests.cs
@@ -5,11 +5,95 @@ namespace katas.test
     [TestFixture]
     public class ScoreKeeperTests
     {
+        ScoreKeeper scoreKeeper;
+
+        [SetUp]
+        public void Setup()
+        {
+            scoreKeeper = new ScoreKeeper();
+        }
+
         [Test]
         public void ShouldReturnDefaultScoreAtTheStart()
         {
-            var scoreKeeper = new ScoreKeeper();
-            Assert.AreSame(scoreKeeper.GetScore(), "000:000");
+            Assert.AreEqual(scoreKeeper.GetScore(), "000:000");
+        }
+
+        [Test]
+        public void ShouldAdd1PointToTeamA()
+        {
+            scoreKeeper.scoreTeamA1();
+            Assert.AreEqual(scoreKeeper.GetScore(), "001:000");
+        }
+
+        [Test]
+        public void ShouldAdd1PointToTeamB()
+        {
+            scoreKeeper.scoreTeamB1();
+            Assert.AreEqual(scoreKeeper.GetScore(), "000:001");
+        }
+
+        [Test]
+        public void ShouldAdd2PointsToTeamA()
+        {
+            scoreKeeper.scoreTeamA2();
+            Assert.AreEqual(scoreKeeper.GetScore(), "002:000");
+        }
+
+        [Test]
+        public void ShouldAdd2PointsToTeamB()
+        {
+            scoreKeeper.scoreTeamB2();
+            Assert.AreEqual(scoreKeeper.GetScore(), "000:002");
+        }
+
+        [Test]
+        public void ShouldAdd3PointsToTeamA()
+        {
+            scoreKeeper.scoreTeamA3();
+            Assert.AreEqual(scoreKeeper.GetScore(), "003:000");
+        }
+
+        [Test]
+        public void ShouldAdd3PointsToTeamB()
+        {
+            scoreKeeper.scoreTeamB3();
+            Assert.AreEqual(scoreKeeper.GetScore(), "000:003");
+        }
+
+        [Test]
+        public void ShouldCorrectlyKeepScore()
+        {
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+            scoreKeeper.scoreTeamA1();
+
+            scoreKeeper.scoreTeamA2();
+            scoreKeeper.scoreTeamA2();
+            scoreKeeper.scoreTeamA2();
+            scoreKeeper.scoreTeamA2();
+
+            scoreKeeper.scoreTeamB1();
+            scoreKeeper.scoreTeamB1();
+            scoreKeeper.scoreTeamB1();
+            scoreKeeper.scoreTeamB1();
+            scoreKeeper.scoreTeamB1();
+            scoreKeeper.scoreTeamB1();
+
+            scoreKeeper.scoreTeamB2();
+
+            scoreKeeper.scoreTeamB3();
+            scoreKeeper.scoreTeamB3();
+            scoreKeeper.scoreTeamB3();
+
+            Assert.AreEqual(scoreKeeper.GetScore(), "018:017");
         }
     }
 }
